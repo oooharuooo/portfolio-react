66b81c0d1eb89b8921f3123a0ce29b29f279ee86
Dat <haruthong@gmail.com>
2021-07-04 17:18:24 -0700

    AboutPage added typing text

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { motion } from "framer-motion";
 import styled from "styled-components";
+import Typewriter from "typewriter-effect";
+
+import WorkingPerson from "../img/WorkingPerson";
 
 const container = {
 	hidden: {
@@ -33,7 +36,39 @@ const AboutPage = () => {
 			animate="visible"
 			exit="exit"
 		>
-			About
+			<motion.div className="container">
+				<Typewriter
+					options={{
+						cursor: "",
+					}}
+					onInit={(typewriter) => {
+						typewriter.typeString("<h1>Hello,</h1>").stop().start();
+					}}
+				/>
+
+				<div className="container-intro">
+					<Typewriter
+						options={{
+							loop: true,
+							delay: 100,
+							wrapperClassName: "type-container",
+						}}
+						onInit={(typewriter) => {
+							typewriter
+								.pauseFor(1000)
+								.typeString("<h2>My name is <span>Dat</span>.</h2>")
+								.pauseFor(1000)
+								.deleteChars(15)
+								.typeString("<h2>I am a <span>Web Developer</span>.</h2>")
+								.pauseFor(1000)
+								.start();
+						}}
+					/>
+				</div>
+			</motion.div>
+			<div className="container-clip-path">
+				<WorkingPerson />
+			</div>
 		</Wrapper>
 	);
 };
@@ -43,4 +78,28 @@ export default AboutPage;
 const Wrapper = styled.div`
 	background-color: white;
 	flex-grow: 1;
+	display: flex;
+
+	position: relative;
+
+	h2 span {
+		color: red;
+	}
+
+	.container {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		&-intro {
+			height: 50px;
+		}
+	}
+	.container-clip-path {
+		background-color: green;
+		clip-path: circle(50% at 100% 10%);
+
+		width: 100%;
+		height: 50%;
+		position: absolute;
+	}
 `;
